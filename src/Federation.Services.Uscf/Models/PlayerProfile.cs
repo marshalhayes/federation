04e156a2376b143fbd3ecbb2d5ace65d18597183
Marshal Hayes <17213165+marshalhayes@users.noreply.github.com>
2022-01-15 22:00:50 -0600

    add online ratings, state, expiration date, last change date, and placement rankings

--- src/Federation.Services.Uscf/Models/PlayerProfile.cs
+++ src/Federation.Services.Uscf/Models/PlayerProfile.cs
@@ -9,4 +9,20 @@ public class PlayerProfile
     public string? RegularRating { get; set; }
     public string? QuickRating { get; set; }
     public string? BlitzRating { get; set; }
+
+    public string? OnlineRegularRating { get; set; }
+    public string? OnlineQuickRating { get; set; }
+    public string? OnlineBlitzRating { get; set; }
+
+    public string? State { get; set; }
+
+    public DateTime? ExpiresOn { get; set; }
+
+    public DateTime? LastChangeDate { get; set; }
+
+    public int? OverallRanking { get; set; }
+    public int? OverallRankingOutOf { get; set; }
+
+    public int? StateRanking { get; set; }
+    public int? StateRankingOutOf { get; set; }
 }
